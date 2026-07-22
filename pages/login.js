36d3b1bfbf4cdf3539f140398db40e595fe73ddb
iiwiiInsider <kdbburns.za@gmail.com>
2026-07-22 14:56:40 +0200

    File modifications for Github Actions to run with no fail.

--- pages/login.js
+++ pages/login.js
@@ -126,7 +126,7 @@ export default function Login({ providers }){
               <div style={{display:'flex',gap:10,flexWrap:'wrap'}}>
                 <button className="btn btn-outline" disabled={loading} onClick={async ()=>{
                   setEmail('admin@local.test')
-                  setPassword(SET PASSWORD Function Below This Line)
+                  setPassword('')
                   await doCredentialsLogin({ email: 'admin@local.test', password: '', source: 'group-admin' })
                 }}>Login as Admin</button>
                 <button className="btn btn-outline" disabled={loading} onClick={async ()=>{
